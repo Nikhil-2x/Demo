051e8839dd2f44ad2c703553675e4f30f01824e9
Nikhil-2x <nikhilyadav101513@gmail.com>
2026-02-02 01:13:21 +0530

    fix-colon issue

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -154,7 +154,7 @@ export function Footer() {
       <div className="mt-12 border-t border-gray-800 pt-8">
         <div className="flex flex-col items-center justify-between gap-4 md:flex-row">
           <p className="text-sm text-gray-400">
-            © {currentYear} Nick's Keyboard. All rights reserved.
+            © {currentYear} Nick&aposs Keyboard. All rights reserved.
           </p>
 
           <div className="flex gap-6">
